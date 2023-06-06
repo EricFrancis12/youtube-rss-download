5f740f3865cdb710d6e68cb7e0b64e2b7f54e6a3
Eric <ericfrancis1@proton.me>
2023-06-06 12:22:47 -0500

    brand-X.json

--- brands/brands.js
+++ brands/brands.js
@@ -8,12 +8,14 @@ const brandsFolderContents = fs.readdirSync('./brands');
 brandsFolderContents.forEach(item => {
     const stats = fs.statSync(`./brands/${item}`);
     if (stats.isDirectory && item !== 'brands.js') {
-        const files = fs.readdirSync(`./brands/${item}`);
-
-        if (files.length > 1 || files[0].slice(-3) !== '.js') return;
-
-        const result = require(`./${item}/${files[0]}`);
-        brands.push(result);
+        //const files = fs.readdirSync(`./brands/${item}`);
+        
+        try {
+            const result = require(`./${item}/${item}.js`);
+            brands.push(result);
+        } catch (err) {
+            console.error(err);
+        }
     }
 });
 
